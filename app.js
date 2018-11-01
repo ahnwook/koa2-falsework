d014a8e38a67d2fde561879172b693733815a321
wook <18900941086@189.cn>
2018-11-01 23:34:55 +0800

    增加session

--- app.js
+++ app.js
@@ -5,38 +5,40 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-session');
 
+const config = require('./lib/data/config');
 const baseRoutes = require('./lib/routes/baseRoutes');
 
 // error handler
 onerror(app)
-
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+    enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
-  extension: 'ejs'
+    extension: 'ejs'
 }))
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+    const start = new Date()
+    await next()
+    const ms = new Date() - start
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
-
+//session
+app.keys = ['my name is wook'];
+app.use(session(config.sessionConfig, app));
 // routes
 app.use(baseRoutes.routes(), baseRoutes.allowedMethods())
-
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+    console.error('server error', err, ctx)
 });
 
 module.exports = app
